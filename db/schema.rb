d6548b1533a3761489f0b87abccc1dd888209f00
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-03 20:41:48 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_054632) do
+ActiveRecord::Schema.define(version: 2020_09_03_114007) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_09_03_054632) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname", null: false
+    t.string "kanji_last_name", null: false
+    t.string "kanji_first_name", null: false
+    t.string "kana_last_name", null: false
+    t.string "kana_first_name", null: false
+    t.date "BirthDay", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
