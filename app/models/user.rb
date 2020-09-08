a8aa78d688a46cb893c3323b4268c88e3b21ef58
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-08 19:38:57 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[a-z\d]+@[a-z\d]+\.[a-z]+\z/i }
   validates :password, presence: true, confirmation: true, format: { with: /\A[a-z\d]{6,}\z/ }
   validates :password_confirmation, presence: true
-  validates :kanji_last_name, :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :kana_last_name, :kana_first_name, presence: true, format: {with: /\A[ァ-ン]+\z/ }
+  validates :kanji_last_name, :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :kana_last_name, :kana_first_name, presence: true, format: { with: /\A[ァ-ン]+\z/ }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"ab00000"}
-    password_confirmation {password}
-    kanji_last_name       {"阿部"}
-    kanji_first_name      {"太郎"}
-    kana_last_name        {"アベ"}
-    kana_first_name       {"タロウ"}
-    BirthDay              {"1995-08-10"}
+    nickname              { 'abe' }
+    email                 { 'kkk@gmail.com' }
+    password              { 'ab00000' }
+    password_confirmation { password }
+    kanji_last_name       { '阿部' }
+    kanji_first_name      { '太郎' }
+    kana_last_name        { 'アベ' }
+    kana_first_name       { 'タロウ' }
+    BirthDay              { '1995-08-10' }
   end
 end
