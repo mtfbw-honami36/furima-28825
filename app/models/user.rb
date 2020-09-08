f6422d0c2013f04c261c45f6d178f04d2153bf5d
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-08 19:30:58 +0900

    userモデルテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, :BirthDay, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[a-z\d]+@[a-z\d]+\.[a-z]+\z/i }
-  validates :password, presence: true, confirmation: true, format: { with: /\A[a-z\d]{6, }\z/ }
+  validates :password, presence: true, confirmation: true, format: { with: /\A[a-z\d]{6,}\z/ }
   validates :password_confirmation, presence: true
   validates :kanji_last_name, :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :kana_last_name, :kana_first_name, presence: true, format: {with: /\A[ァ-ン]+\z/ }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {"abe"}
     email                 {"kkk@gmail.com"}
-    password              {"0000000"}
+    password              {"ab00000"}
     password_confirmation {password}
     kanji_last_name       {"阿部"}
     kanji_first_name      {"太郎"}
     kana_last_name        {"アベ"}
     kana_first_name       {"タロウ"}
+    BirthDay              {"1995-08-10"}
   end
 end
