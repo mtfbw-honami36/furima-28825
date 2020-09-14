ffcfb5be9d6ea66ccd6aea732b22dee6cb41e56e
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-14 16:24:54 +0900

    テストコード実行、Rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  
+
   belongs_to_active_hash :genre
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :delivery_day
-  
+
   has_one_attached :image
 
   validates :name, :text, :genre, :item_condition, :shipping_cost, :ship_from, :delivery_day, presence: true
-  validates :genre_id, :item_condition_id, :shipping_cost_id, :ship_from_id, :delivery_day_id, numericality: { other_than: 1 }
-  validates :price, numericality: { only_integer: true }, length: { in: 300..9999999 }
-  validates :image, null: false
+  validates :genre_id, :item_condition_id, :shipping_cost_id, :ship_from_id, :delivery_day_id, numericality: { other_than: 1, message: 'Select' }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+  validates :image, presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { 'テント' }
+    text              { 'キャンプにぴったりです' }
+    genre_id          { 9 }
+    item_condition_id { 3 }
+    shipping_cost_id  { 2 }
+    ship_from_id      { 23 }
+    delivery_day_id   { 2 }
+    price             { 6000 }
+    association :user
   end
 end
