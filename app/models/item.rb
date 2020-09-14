cb33b0f0f3e28bf9892ad23976cdd22314a01103
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-14 17:01:53 +0900

    with_optionsでバリデーションをまとめた

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,11 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  validates :name, :text, :genre, :item_condition, :shipping_cost, :ship_from, :delivery_day, presence: true
-  validates :genre_id, :item_condition_id, :shipping_cost_id, :ship_from_id, :delivery_day_id, numericality: { other_than: 1, message: 'Select' }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  with_options presence: true do
+  validates :name, :text, :genre, :item_condition, :shipping_cost, :ship_from, :delivery_day,:image
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   validates :price, numericality: { only_integer: true, message: 'Half-width number' }
-  validates :image, presence: true
+  end
+  validates :genre_id, :item_condition_id, :shipping_cost_id, :ship_from_id, :delivery_day_id, numericality: { other_than: 1, message: 'Select' }
+
 end
