a0e058e08665e9c471e1905a4ca74ab6555f623a
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-17 16:51:18 +0900

    出品者は購入ページのURLにとべない

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,9 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    if @item.user_id == current_user
+      redirect_to root_path
+    end
   end
   
   def create
