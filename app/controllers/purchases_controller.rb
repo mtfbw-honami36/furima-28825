922fc4ea1d3abbbab74dc3c0d7dcd7c562070698
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-09-21 13:19:49 +0900

    クレジットカード決済機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -11,4 +11,16 @@ class PurchasesController < ApplicationController
   
   def create
   end
+
+  private
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: purchase_params[:price],
+      card: purchase_params[:token],
+      currency:'jpy'
+    )
+  end
+
 end
